3063da7b558adfc4b474b1565b5228168ab2a8d9
mcgue <mcgu@protonmail.com>
2022-07-24 18:16:50 -0700

    add instructions

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # Text-Based Adventure Game
 
+# Introduction
 print("Let\'s play Adventure")
 
+# Obtain player's name
 player_name = input("What do you want to call your character?")
 
+# Tell rules
+
+print("The object is to find the key and unlock the jeweled box."
+      "The key is in one of the rooms")
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
