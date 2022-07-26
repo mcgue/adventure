4e165745802dc94f93107fe84bc9f74324317a7e
mcgue <mcgu@protonmail.com>
2022-07-26 14:34:10 -0700

    change order

--- main.py
+++ main.py
@@ -3,13 +3,12 @@
 # Introduction
 print("Let\'s play Adventure")
 
-# Obtain player's name
-player_name = input("What do you want to call your character?")
-
-# Tell rules
-
+# Tell objective
 print("The object is to find the key and unlock the jeweled box."
       "The key is in one of the rooms")
+
+# Obtain player's name
+player_name = input("What do you want to call your character?")
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
