1f737ef80f2b5dabd238b7f756addd7fb228b300
mcgue <mcgu@protonmail.com>
2022-07-30 17:23:27 -0700

    remove unneeded comment

--- main.py
+++ main.py
@@ -18,5 +18,3 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi(player_name)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
