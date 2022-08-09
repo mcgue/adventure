baafedd67320a739146083a7ebac02cd806d2a9a
mcgue <mcgu@protonmail.com>
2022-08-09 16:22:36 -0700

    create Gold subclass

--- items.py
+++ items.py
@@ -7,3 +7,11 @@ class Item():
 
     def __str__(self):
         return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value)
+
+# Child class of Item
+class Gold(Item):
+    def __init__(self, amt):
+        self.amt = amt
+        super().__init__(name="Gold",
+                         description="A round coin with {} stamped on the front.".format(str(self.amt)),
+                         value=self.amt)
