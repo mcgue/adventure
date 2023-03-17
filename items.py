d439aee375b73f601de615249c77f1b29d143adf
mcgue <mcgu@protonmail.com>
2023-03-17 09:17:00 -0700

    move classes to separate file

--- items.py
+++ items.py
@@ -1,17 +1,28 @@
-# Create an Item class - the base for all items
+# Base for all items
 class Item():
+    """Base class for all items"""
     def __init__(self, name, description, value):
         self.name = name
         self.description = description
         self.value = value
 
     def __str__(self):
-        return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value)
+        return f"{self.name}\n=====\n{self.description}\nValue: {self.value}\n"
 
-# Child class of Item
-class Gold(Item):
-    def __init__(self, amt):
-        self.amt = amt
+# Items of value
+class Jewel(Item):
+    def __init__(self, color):
+        self.color = color
         super().__init__(name="Gold",
-                         description="A round coin with {} stamped on the front.".format(str(self.amt)),
-                         value=self.amt)
+                         description="A {} jewel.".format(str(self.color)),
+                         value=self.color)
+
+# Helps in game, used is times can help, help is how much can help
+class Partner:
+    def __init__(self, name, used, help):
+        self.name = name
+        self.used = used
+        self.help = help
+
+    def is_alive(self):
+        return self.used > 0
--- main.py
+++ main.py
@@ -6,34 +6,6 @@
 
 import time
 
-# Base for all items
-class Item():
-    """Base class for all items"""
-    def __init__(self, name, description, value):
-        self.name = name
-        self.description = description
-        self.value = value
-
-    def __str__(self):
-        return f"{self.name}\n=====\n{self.description}\nValue: {self.value}\n"
-
-class Jewel(Item):
-    def __init__(self, color):
-        self.color = color
-        super().__init__(name="Gold",
-                         description="A {} jewel.".format(str(self.color)),
-                         value=self.color)
-
-# Helps in game, used is times can help, help is how much can help
-class Partner:
-    def __init__(self, name, used, help):
-        self.name = name
-        self.used = used
-        self.help = help
-
-    def is_alive(self):
-        return self.used > 0
-
 # Tell objective
 # print("The object is to ?? ")
 
